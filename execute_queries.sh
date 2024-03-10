86cbc036575f01a986ebd245625066b5c2f108fd
Pei Mo <pei@luna.inf.ed.ac.uk>
2024-03-10 15:42:08 +0000

    update script

--- execute_queries.sh
+++ execute_queries.sh
@@ -7,9 +7,9 @@ mkdir -p result/
 for i in $(eval echo {1.."${iteration}"}); do
   for sql in "${dir}"/*; do
     echo "execute ${sql}";
-    psql -U imdb -f "${sql}" 2>&1|tee -a skinner_explained_tpch-3_${i}.txt;
-    #psql -U imdb -f "${sql}";
+    psql -U imdb -d imdb -f "${sql}" 2>&1|tee -a skinner_explained_imdb_${i}.txt;
+    #psql -U imdb -d -f "${sql}";
   done
 done
 
-mv skinner_explained_tpch-3_* result/.
+mv skinner_explained_imdb_* result/.
