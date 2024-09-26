77bd4480e5a534a9cec7b2cfe0359a41c40dd19f
pei <ds1231h@gmail.com>
2024-09-26 18:48:14 +0100

    queries without AS

--- queries_without_AS/14a.sql
+++ queries_without_AS/14a.sql
@@ -1,21 +1,21 @@
-SELECT MIN(mi_idx.info) AS rating,
-       MIN(t.title) AS northern_dark_movie
-FROM info_type AS it1,
-     info_type AS it2,
-     keyword AS k,
-     kind_type AS kt,
-     movie_info AS mi,
-     movie_info_idx AS mi_idx,
-     movie_keyword AS mk,
-     title AS t
-WHERE it1.info = 'countries'
-  AND it2.info = 'rating'
-  AND k.keyword IN ('murder',
+SELECT MIN(movie_info_idx.info) AS rating,
+       MIN(title.title) AS northern_dark_movie
+FROM info_type,
+     info_type,
+     keyword,
+     kind_typet,
+     movie_info,
+     movie_info_idx,
+     movie_keyword,
+     title
+WHERE info_type.info = 'countries'
+  AND info_type.info = 'rating'
+  AND keyword.keyword IN ('murder',
                     'murder-in-title',
                     'blood',
                     'violence')
-  AND kt.kind = 'movie'
-  AND mi.info IN ('Sweden',
+  AND title.kind = 'movie'
+  AND movie_info.info IN ('Sweden',
                   'Norway',
                   'Germany',
                   'Denmark',
@@ -25,16 +25,16 @@ WHERE it1.info = 'countries'
                   'German',
                   'USA',
                   'American')
-  AND mi_idx.info < '8.5'
-  AND t.production_year > 2010
-  AND kt.id = t.kind_id
-  AND t.id = mi.movie_id
-  AND t.id = mk.movie_id
-  AND t.id = mi_idx.movie_id
-  AND mk.movie_id = mi.movie_id
-  AND mk.movie_id = mi_idx.movie_id
-  AND mi.movie_id = mi_idx.movie_id
-  AND k.id = mk.keyword_id
-  AND it1.id = mi.info_type_id
-  AND it2.id = mi_idx.info_type_id;
+  AND movie_info_idx.info < '8.5'
+  AND title.production_year > 2010
+  AND title.id = title.kind_id
+  AND title.id = movie_info.movie_id
+  AND title.id = movie_keyword.movie_id
+  AND title.id = movie_info_idx.movie_id
+  AND movie_keyword.movie_id = movie_info.movie_id
+  AND movie_keyword.movie_id = movie_info_idx.movie_id
+  AND movie_info.movie_id = movie_info_idx.movie_id
+  AND keyword.id = movie_keyword.keyword_id
+  AND info_type.id = movie_info.info_type_id
+  AND info_type.id = movie_info_idx.info_type_id;
 
